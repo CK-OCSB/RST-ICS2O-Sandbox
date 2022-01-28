01cc02ef56bb6b8edaf45cd5596e393e9aeaaa4e
CK-OCSB <97174126+CK-OCSB@users.noreply.github.com>
2022-01-28 13:36:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# NAME OF AUTHOR: Carter King (CK-OCSB)
+# NAME OF THE PROGRAM: Math Practice  
+# DATE OF CREATION: 2022-01-28 ,  
+# PURPOSE OF PROGRAM: To provide a serises of math problems to the user. The user will chose the type of questions as well as the difficulty.  
+
+# VARIABLE DEFINITION
+import time
+timeAmmount = 
+while timeAmmount :
+    mins = timeAmmount // 60
+    secs = timeAmmount % 60 
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    time.sleep(1)
+    timeAmmount-= 1
+print("timer over")
+# INPUT
+
+# PROCESSING
+
+# OUTPUT
